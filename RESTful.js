ac978b17f81f1bffb253242467bddc33324c7c20
Firat Sertgoz <Firat.Sertgoz-CIC.Netherlands@ibm.com>
2017-11-29 11:55:57 +0100

    Fixes

--- RESTful.js
+++ RESTful.js
@@ -2,10 +2,12 @@
 var express = require('express'),
 app = express(),
 port = process.env.PORT || 3000;
-
+bodyParser = require('body-parser');
 var routes = require('./routers/routers'); //importing route
+routes(app);
 console.log('RESTful API server started on: ' + port);
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(port);
 
